5c45ed3c7534950be1cef7de87de5ae347146d41
RokasTverijonas <tverijonasr@gmail.com>
2026-02-04 21:42:54 +0200

    Struktūra

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+struct studentas{
+    std::string vardas, pavarde;
+    int* pazymiai;
+    int egzaminas;
+    int rezultatas;
+};
+
+int main() {
+    studentas A;
+}
