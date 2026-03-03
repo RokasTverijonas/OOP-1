ac33fa433f1bb34a95544b4e4852849bf48a5649
RokasTverijonas <tverijonasr@gmail.com>
2026-03-03 10:08:17 +0200

    Add isvedimas header file

---
+++ programa/isvedimas.h
@@ -0,0 +1,11 @@
+#ifndef ISVEDIMAS_H
+#define ISVEDIMAS_H
+
+#include "studentas.h"
+
+
+void rikiavimas(std::vector<studentas>& A);
+void spausdinimas(std::vector<studentas>& A);
+void failoSpausdinimas(std::vector<studentas>& A)
+
+#endif
