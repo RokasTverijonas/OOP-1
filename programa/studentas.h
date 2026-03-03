46cfd07e195793ede704c1b63b980710621715b4
RokasTverijonas <tverijonasr@gmail.com>
2026-03-03 09:32:23 +0200

    Add studentas struct header file

---
+++ programa/studentas.h
@@ -0,0 +1,14 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+#include <string>
+#include <vector>
+
+struct studentas {
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> nd;
+    int egzaminas;
+}
+
+#endif
