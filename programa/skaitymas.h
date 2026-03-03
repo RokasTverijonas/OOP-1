14b3a28fb3987744eeaf6023f3cefedd44ce60e4
RokasTverijonas <tverijonasr@gmail.com>
2026-03-03 09:53:21 +0200

    Add skaitymas header file

---
+++ programa/skaitymas.h
@@ -0,0 +1,10 @@
+#ifndef SKAITYMAS_H
+#define SKAITYMAS_H
+
+#include <vector>
+#include "studentas.h"
+
+void skaitymas(std::vector<studentas>& A, std::string failas);
+void skaitymoTestai(std::vector<studentas>& A, std::string failas);
+
+#endif
