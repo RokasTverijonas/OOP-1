2df1d94a47e178aa08f44eed96df4d2eca48a1e1
RokasTverijonas <tverijonasr@gmail.com>
2026-03-03 09:39:05 +0200

    Add skaiciavimai header file

---
+++ programa/skaiciavimai.h
@@ -0,0 +1,10 @@
+#ifndef SKAICIAVIMAI_H
+#define SKAICIAVIMAI_H
+
+#include "studentas.h"
+
+double vidurkis(const studentas& A);
+double mediana(const studentas& A);
+double galutinis(const studentas& A, double balai);
+
+#endif
